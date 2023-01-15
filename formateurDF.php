953c958a15378abe05170cef4e57730e5727fe4d
MOHAMMED TAHER <100471051+samhamed@users.noreply.github.com>
2023-01-15 11:50:17 +0100

    form delet + update

---
+++ formateurDF.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="http://localhost:81/api/formateurD.php" method="POST">
+    <div>
+      <label for="form1Example1">Id</label>
+      <input type="numbre" id="form1Example1"  name="id"/>
+  </div>
+  <button type="submit">DELET</button>
+</form>
+</body>
+</html>
